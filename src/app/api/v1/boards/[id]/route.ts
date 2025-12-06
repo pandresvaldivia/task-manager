51a7dde7c8430c0c2e64ee3a5a029acda1001759
pandresvaldivia <pandresdev@gmail.com>
2025-12-06 18:51:17 -0500

    fix: move update board method to the right endpoint

--- src/app/api/v1/boards/[id]/route.ts
+++ src/app/api/v1/boards/[id]/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
 import { Prisma } from '@/generated/prisma/client';
+import { StatusScalarWhereInput } from '@/generated/prisma/models';
+import { isValidString } from '@/modules/shared/helpers/string';
 
 export async function GET(
   _request: NextRequest,
@@ -8,7 +10,7 @@ export async function GET(
 ) {
   const { id } = await context.params;
 
-  if (!id) {
+  if (!isValidString(id)) {
     return NextResponse.json({ error: 'Missing id' }, { status: 400 });
   }
 
@@ -50,7 +52,7 @@ export async function DELETE(
 ) {
   const { id } = await context.params;
 
-  if (!id) {
+  if (!isValidString(id)) {
     return NextResponse.json({ error: 'Missing id' }, { status: 400 });
   }
 
@@ -82,6 +84,125 @@ export async function DELETE(
     return NextResponse.json(
       { error: 'Failed to delete board' },
       { status: 500 }
+    );
+  }
+}
+
+export async function PUT(
+  request: NextRequest,
+  context: RouteContext<'/api/v1/boards/[id]'>
+) {
+  const { id } = await context.params;
+  const { name, statuses = [] } = await request.json();
+
+  if (!isValidString(id)) {
+    return NextResponse.json(
+      {
+        error: 'Board id is required',
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  if (!isValidString(name)) {
+    return NextResponse.json(
+      {
+        error: 'Board name is required',
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  if (statuses && !Array.isArray(statuses)) {
+    return NextResponse.json(
+      {
+        error: 'Statuses must be a valid JSON array',
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  const statusesList: { id?: string; name: string }[] = [];
+  const existingIds: StatusScalarWhereInput[] = [];
+
+  for (const status of statuses) {
+    if (!isValidString(status.name)) {
+      return NextResponse.json(
+        {
+          error: 'All statuses must have a name',
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    if (status.id) {
+      existingIds.push({ id: status.id });
+    }
+
+    statusesList.push({
+      id: status.id,
+      name: status.name.trim(),
+    });
+  }
+
+  try {
+    const board = await prisma.board.update({
+      where: { id },
+      data: {
+        name: name.trim(),
+        statuses: {
+          deleteMany: {
+            boardId: id,
+            NOT: existingIds,
+          },
+          upsert: statusesList.map((status) => ({
+            where: { id: status.id ?? '' },
+            create: { name: status.name },
+            update: { name: status.name },
+          })),
+        },
+      },
+    });
+
+    return NextResponse.json(
+      {
+        data: board,
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (e) {
+    console.log({ error: e });
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      console.log(e.code);
+      if (e.code === 'P2025') {
+        return NextResponse.json(
+          {
+            error: 'Board not found',
+          },
+          {
+            status: 404,
+          }
+        );
+      }
+    }
+
+    return NextResponse.json(
+      {
+        error: 'Failed to update board',
+      },
+      {
+        status: 500,
+      }
     );
   }
 }
